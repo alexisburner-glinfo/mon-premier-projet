3169167b3ea373a9b7b86bc232d501204c0696d3
Alexis <alexis.burner@glinfo.fr>
2026-05-29 16:45:49 +0200

    Ajout d'une page About

---
+++ src/app/about.tsx
@@ -0,0 +1,24 @@
+
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className={styles.intro}>
+          <h1>A propos !</h1>
+          <p>Super site de test ...</p>
+        </div>
+      </main>
+    </div>
+  );
+}
